b123c70303526aba1eda8fd7380e518104375a5a
GuardLDW <303899225@qq.com>
2016-09-15 20:57:15 +0800

    编写了绘制金字塔的方法，未解决的问题:1.console.log打印出语句后自动换行  2.不知道如何从键盘获取输入

--- 14081206/main.js
+++ 14081206/main.js
@@ -1,1 +1,15 @@
-console.log("helloworld");
+var i = 0;
+var j = 0;
+var n = 2;
+drawPyramid(n);
+function drawPyramid(n) {
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n - i; j++) {
+            console.log("");
+        }
+        for (j = 0; j < 2 * i + 1; j++) {
+            console.log("*");
+        }
+        console.log("\n");
+    }
+}
--- 14081206/main.ts
+++ 14081206/main.ts
@@ -1,1 +1,20 @@
-console.log("helloworld");
+
+ var i : number = 0;
+ var j : number = 0;
+ var n : number = 2;
+
+
+drawPyramid(n);
+
+
+ function drawPyramid(n : number) : void {
+     for(i = 0 ; i < n; i++){
+         for(j = 0; j < n - i; j++){
+             console.log("");
+         }
+         for(j = 0; j < 2 * i + 1; j++){
+             console.log("*");
+         }
+         console.log("\n");
+     }
+ }
